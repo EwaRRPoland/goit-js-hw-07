e274040e1178a4a891c643c1acc953a7f217f23e
Ewa <ewarostrz@gmail.com>
2024-05-31 13:40:33 +0200

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,7 @@
+// Wybieramy element o id "ingredients" z dokumentu HTML. Wynik jest zapisywany w zmiennej "ulList".
+const ulList = document.getElementById('ingredients');
+
+// Tworzymy tablicę składników.
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +10,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// Dla każdego składnika w tablicy wykonujemy następujące operacje.
+ingredients.forEach(ingredient => {
+  // Tworzymy nowy element "li" i zapisujemy go w zmiennej "liElement".
+  const liElement = document.createElement("li");
+
+  // Ustawiamy tekst elementu "li" na nazwę składnika.
+  liElement.textContent = ingredient;
+
+  // Dodajemy klasę "item" do elementu "li".
+  liElement.classList.add("item");
+
+  // Dodajemy element "li" do listy "ul".
+  ulList.append(liElement);
+
+});
