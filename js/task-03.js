d496cb4c00c5f1dc105e5d510de3091b8e0a8f4d
Ewa <ewarostrz@gmail.com>
2024-05-31 15:30:04 +0200

    task 3 done

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,7 @@
+
+// Wybieramy pierwszy element o klasie "gallery" z dokumentu HTML. Wynik jest zapisywany w zmiennej "galleryUl".
+const galleryUl = document.getElementsByClassName('gallery')[0];
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +16,16 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// Tworzymy łańcuch HTML dla wszystkich obrazów.
+let imagesHTML = '';
+images.forEach(image => {
+  imagesHTML += `
+    <li class="item">
+      <img src="${image.url}" alt="${image.alt}">
+    </li>
+  `;
+});
+
+// Dodajemy cały HTML do listy "ul" za pomocą metody "insertAdjacentHTML".
+galleryUl.insertAdjacentHTML('beforeend', imagesHTML);
